1026b0bc4aa61d16475422c2ada222b263dbb438
lotsofluck <anitabangali@yahoo.com>
2021-03-04 22:00:44 -0600

    fixed env

--- events_app/config.py
+++ events_app/config.py
@@ -6,6 +6,5 @@ load_dotenv()
 
 class Config(object):
     """Set environment variables."""
-
-    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
